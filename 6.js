e7467cdd6dfa41c93877923dc47e73605655ee0c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-05 07:37:23 +0300

    - promise all

--- 6.js
+++ 6.js
@@ -18,36 +18,50 @@ setTimeout(function () {
 },2000)*/
 
 // ми отримали функцію Колбек в конструкторі класа проміс
-const p = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log('Preparing data...')
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data...')
+//
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'working'
+//         }
+//         resolve(backendData)
+//     }, 2000)
+// })
+//
+// p.then((data) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.modified = true;
+//             resolve(data)
+//             //        console.log('Data received', backendData)
+//         }, 2000)
+//     })
+// }).then(clientData => {
+//         console.log('Data received', clientData)
+//         clientData.fromPromise = true;
+//         return clientData
+//     }).then(data => {
+//     console.log('Modified data', data)
+// })
+//     .catch(err => console.error('Error:',err))
+//     .finally(()=>{
+//         console.log('Finally')
+//     })
 
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            status: 'working'
-        }
-        resolve(backendData)
-    }, 2000)
-})
 
-p.then((data) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.modified = true;
-            resolve(data)
-            //        console.log('Data received', backendData)
-        }, 2000)
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), ms)
     })
-}).then(clientData => {
-        console.log('Data received', clientData)
-        clientData.fromPromise = true;
-        return clientData
-    }).then(data => {
-    console.log('Modified data', data)
-})
-    .catch(err => console.error('Error:',err))
-    .finally(()=>{
-        console.log('Finally')
-    })
+}
 
+// sleep(2000).then(() => console.log('after 2 seconds'));
+// sleep(5000).then(() => console.log('after 5 seconds'));
+
+Promise.all([sleep(2000), sleep(5000)])
+.then(()=>{
+    console.log('All promises')
+})
