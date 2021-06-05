373e62e3d5ded577168fa70b9d474eb0c652a563
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-05 15:37:03 +0300

    - promise all

--- 7.js
+++ 7.js
@@ -1,31 +1,39 @@
-console.log('Request data...')
+// console.log('Request data...')
+//
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data...')
+//
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'is working'
+//         }
+//         resolve(backendData)
+//     }, 2000)
+// })
+//
+// p.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.modified = true;
+//             reject(data)
+//         }, 2000)
+//     })
+// }).catch(err => console.log('Error: ', err))
+//   .then((clientData) => {
+//         console.log('Data received: ', clientData)
+//         clientData.fromPromise = true
+//         return clientData
+//     })
+//     .then(data => {
+//     console.log('Modified', data)
+//         })
 
-const p = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log('Preparing data...')
+const sleep = ms => {
+   return new Promise(resolve =>
+       setTimeout(() =>resolve(), ms))
+}
 
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            status: 'is working'
-        }
-        resolve(backendData)
-    }, 2000)
-})
-
-p.then(data => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.modified = true;
-            reject(data)
-        }, 2000)
-    })
-}).catch(err => console.log('Error: ', err))
-  .then((clientData) => {
-        console.log('Data received: ', clientData)
-        clientData.fromPromise = true
-        return clientData
-    })
-    .then(data => {
-    console.log('Modified', data)
-        })
+sleep(2000).then(() => console.log('2 seconds timeout'))
+sleep(5000).then(() => console.log('5 seconds timeout'))
