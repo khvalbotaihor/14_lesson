f76327763bc7c05e9f183c64e0cc356d1884265d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-05 06:38:00 +0300

    - inside investigation of async await

--- 5.js
+++ 5.js
@@ -1,8 +1,14 @@
 console.log('Start 1')
 console.log('Start 2')
 
+function timeout2sec(){
+    console.log('timeout2sec')
+}
+
 setTimeout(function () {
-    console.log('Inside tomeout, after 2 seconds')
-}, 2000)
+    console.log('Inside timeout, after 2 seconds')
+}, 5000)
+
+setTimeout(timeout2sec, 2000)
 
 console.log('End')
