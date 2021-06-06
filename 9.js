d90c95b3b08f8cba41fcb2337e850a7cbf2f7a18
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-06 14:50:02 +0300

    - repeat promises before async await

--- 9.js
+++ 9.js
@@ -1,11 +1,21 @@
 const fetchData = () => {
-    setTimeout(() => {
-
-    },1500)
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!')
+        }, 1500)
+    })
+    return promise
 }
 
 setTimeout(() => {
     console.log('Times is done')
+    fetchData().then(text => {
+        console.log(text);
+       return fetchData()
+    })
+    .then(text2 => {
+        console.log(text2)
+    })
 }, 2000)
 
 console.log('Hello')
