b14edda2f781b2967debc440532fd34293d7b2e7
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-06 14:42:04 +0300

    - repeat promises before async await

--- 8.js
+++ 8.js
@@ -1,18 +1,34 @@
 const delay = ms => {
-    return new Promise(r =>
-        setTimeout(() => r(), ms))
+    return new Promise((resolve, reject) =>
+        setTimeout(() => resolve(),ms))
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos'
+//
+// const fetchTodo = () => {
+//     console.log('Data is requested in server...')
+//   return   delay(3000)
+//         .then(() => fetch(url))
+//         .then(response => response.json())
+// }
+//
+// fetchTodo()
+//     .then(data => console.log('Server data: ', data))
+//     .catch(e => console.error('Error: ', e))
 
-function fetchData() {
-  console.log('Data is requested in server...')
-  return delay(3000)
-      .then(() => fetch(url))
-           .then(response => response.json())
-
+async function fetchAsyncTodo() {
+    console.log('Data is requested in server...')
+    try {
+        await delay(3000)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log('Server data: ', data)
+    }catch (e) {
+        console.error('Error: ', e)
+    }finally {
+        await delay(5000)
+        console.log('Finally')
+    }
 }
 
-fetchData()
-    .then(data => console.log('Server data: ', data))
-    .catch(e => console.error('Error: ', e))
+fetchAsyncTodo()
---
+++ 9.js
@@ -0,0 +1,12 @@
+const fetchData = () => {
+    setTimeout(() => {
+
+    },1500)
+}
+
+setTimeout(() => {
+    console.log('Times is done')
+}, 2000)
+
+console.log('Hello')
+console.log('Hi')
