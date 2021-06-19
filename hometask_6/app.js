2d71c611d258daf94cbb59460d175263bb77cc6f
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-06-19 11:29:44 +0300

    - node js - nodemon - express

--- hometask_6/app.js
+++ hometask_6/app.js
@@ -1,18 +1,18 @@
-const http = require('http')
-
 const express = require('express')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware")
+app.use('/', (req, res, next) => {
+    console.log("In start")
     next()
 })
+app.use('/add-product', (req, res, next) => {
+    console.log("In the add-product middleware")
+    res.send('<h1>Hello from add-product</h1>')
+})
 
-app.use((req, res, next) => {
-    console.log("In the NEXT middleware")
+app.use('/', (req, res, next) => {
+    console.log("In the express middleware")
     res.send('<h1>Hello from express</h1>')
 })
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000)
