87e802510a15696cc5e1871aa8b539d263043819
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-07-20 16:24:43 +0300

    - adding model - product model

--- express_13/main.js
+++ express_13/main.js
@@ -1,36 +1,79 @@
-function findSmallest(array) {
-    let smallestIndex = 0
-    let smallestElement = array[0]
-
-    for (var i = 1; i < array.length; i++){
-        if (smallestElement > array[i]){
-            smallestElement = array[i]
-            smallestIndex = i
-        }
+function sum(array) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+        total += array[i]
     }
-    return smallestIndex
+    return total
 }
 
-function selectionSort(arr) {
-    if (!arr.length) return []
+sum([1, 2, 3])
 
-    let smallest = arr.splice(findSmallest(arr), 1)
-    return smallest.concat(selectionSort(arr))
+function sumReduce(array) {
+    return array.reduce(function (curr, prev) {
+        return curr + prev
+    })
 }
 
-selectionSort([5, 3, 6, 2, 10])
+sumReduce([1, 2, 3])
 
-function countDown(value) {
-    if (value <= 0) return
-    countDown(value - 1)
+function sumRecursive(array) {
+    if (array.length === 1) return array[0]
+    return array[0] + sumRecursive(array.slice(1))
 }
 
-countDown(5)
+sumRecursive([1, 2, 3])
 
-function Factorial(i) {
-    if (i <= 0) return 1
-    return i * Factorial(i - 1)
-
+function recursiveSum(array) {
+    if (array.length === 0) return 0
+    return 1 + recursiveSum(array.slice(1))
 }
 
-console.log(Factorial(5))
+recursiveSum([1, 2, 3, 4, 5, 6, 0])
+
+function alternativeSolutionMax(array, max = 0) {
+    return array.length === 0
+        ? max
+        : alternativeSolutionMax(array.slice(1),
+            array[0] > max ? array[0] : max
+        )
+}
+
+alternativeSolutionMax([1, 33, 22, 54, 11, 7, 99])
+
+function max(array) {
+    if (array.length === 2) return array[0] > array[1] ? array[0] : array[1]
+    let sub_max = max(array.slice(1))
+    return array[0] > sub_max
+        ? array[0]
+        : sub_max
+}
+
+max([77, 5, 8, 13, 4, 55])
+
+function quicksort(array) {
+    if (array.length < 2) return array
+    let pivot = array[0]
+    console.log({pivot})
+    let less = array.slice(1).filter(function (el) {
+        return el <= pivot
+    })
+    console.log({less})
+    let greater = array.slice(1).filter(function (el) {
+        return el > pivot
+    })
+    console.log({greater})
+    return quicksort(less).concat([pivot], quicksort(greater))
+}
+
+console.log(quicksort([1,2,6,5]))
+
+
+
+
+
+
+
+
+
+
+
